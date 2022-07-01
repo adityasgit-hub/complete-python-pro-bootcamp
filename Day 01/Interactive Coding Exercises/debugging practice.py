b19c64a661b4551c59d43b627506cd0f88228202
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-01 06:46:27 +0530

    Create debugging practice.py

---
+++ Day 01/Interactive Coding Exercises/debugging practice.py
@@ -0,0 +1,18 @@
+#Fix the code below 👇
+
+print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
+
+
+#Fixed code
+
+#missing quotation mark after the opening parenthesis.
+print("Day 1 - String Manipulation")
+#either convert the string's double-inverted commas to single-inverted commas or the plus operator's double-inverted commas to single-inverted commas.
+print('String Concatenation is done with the "+" sign.')
+#no identation required.
+print('e.g. print("Hello " + "world")')
+#double paranthesis after the word print.
+print("New lines can be created with a backslash and n.")
