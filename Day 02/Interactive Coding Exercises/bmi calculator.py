cefa789d61c72ac9ce769e758e17f5f91aa9760c
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-02 07:35:41 +0530

    Update bmi calculator.py

--- Day 02/Interactive Coding Exercises/bmi calculator.py
+++ Day 02/Interactive Coding Exercises/bmi calculator.py
@@ -1,5 +1,4 @@
 height = input("Enter your height in m: ")
 weight = input("Enter your weight in kg: ")
 bmi = int(weight) / float(height) ** 2
-bmi_as_int = int(bmi)
-print(bmi_as_int)
+print(int(bmi))
