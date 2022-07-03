d79cc2c1a3a50952f66a7756fe0157990df06e95
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-03 18:20:32 +0530

    Update treasure island.py

--- Day 03/treasure island.py
+++ Day 03/treasure island.py
@@ -24,9 +24,9 @@ print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right".\n').lower()
 if choice1 == "left":
-  choice2 = input('You come to a lake. There is an island in the middle of the lake. Type "wait" to wait for the boat. Type "swim" to swim across.\n')
+  choice2 = input('You come to a lake. There is an island in the middle of the lake. Type "wait" to wait for the boat. Type "swim" to swim across.\n').lower()
   if choice2 == "wait":
-    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow, and one blue. Which color do you choose?\n")
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow, and one blue. Which color do you choose?\n").lower()
     if chouce3 == "red":
       print("It's a room full of fire. Game Over!")
     elif choice3 == "yellow":
