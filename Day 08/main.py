3159005b4e91988f5772ec340785581253f766ce
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-19 21:12:07 +0530

    Update main.py

--- Day 08/main.py
+++ Day 08/main.py
@@ -5,9 +5,26 @@ def greet():
   
 greet()
 
-#function with input
+# function with input
 
 def greet_with_name(name):
   print(f"Hello, I'm {name}")
   
 greet_with_name("Aditya")
+
+# function with more than one input
+
+
+# this function is by default having positional arguments 
+def greet_with(name, location):
+  print(f"Hello! {name}")
+  print(f"What is it like in {location}")
+  
+greet_with("Aditya", "Dehradun")
+
+# same above function with keyword arguments
+def greet_with(name, location):
+  print(f"Hello! {name}")
+  print(f"What is it like in {location}")
+  
+greet_with(location = "Dehradun", name = "Aditya")
