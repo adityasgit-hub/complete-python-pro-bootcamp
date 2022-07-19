2c7392b7e87844d82aebca8971b71fbf510f39ba
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-07-19 20:57:49 +0530

    Create main.py

---
+++ Day 08/main.py
@@ -0,0 +1,13 @@
+def greet():
+  print("Hello, there!")
+  print("How are you?")
+  print("Isn't the weather nice today!")
+  
+greet()
+
+#function with input
+
+def greet_with_name(name):
+  print(f"Hello, I'm {name}")
+  
+greet_with_name("Aditya")
