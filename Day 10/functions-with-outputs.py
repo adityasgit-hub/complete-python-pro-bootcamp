07045af8ceca42f15620958cd410c96c70f6e338
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-08-24 07:23:34 +0530

    Update functions-with-outputs.py

--- Day 10/functions-with-outputs.py
+++ Day 10/functions-with-outputs.py
@@ -5,5 +5,6 @@ def format_name(f_name, l_name):
   l_name = l_name.title()
   return (f"{f_name} {l_name}")
     
-name = format_name("aDiTYa", "rAwAt")
+# name = format_name("aDiTYa", "rAwAt")
+name = format_name(input("Enter your first name: "), input("Enter your last name: "))
 print(name)
