1b0ff6190de158bff6a394e84e3f5cfa986f0f5a
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-08-28 10:31:31 +0530

    Create constants.py

---
+++ Day 12/constants.py
@@ -0,0 +1,6 @@
+# Global Constants
+
+# In order to declare a constant in Python, declare the constants variables in Uppercase.
+PI = 3.14159
+URL = "https://www.google.com"
+TWITTER_HANDLE = "@adityatwts"
