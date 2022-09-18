32efe5c8baf59e87b63d538e7823955e3dc95dcf
Aditya Rawat <107842826+adityasgit-hub@users.noreply.github.com>
2022-09-18 09:04:04 +0530

    Update main.py

--- Day 14/main.py
+++ Day 14/main.py
@@ -1,34 +1,14 @@
-# Generate a random account from the game data.
-
-# Format account data into printable format.
-
-# Ask user for a guess.
-
-# Check if user is correct.
-## Get follower count.
-## If Statement
-
-# Feedback.
-
-# Score Keeping.
-
-# Make game repeatable.
-
-# Make B become the next A.
-
-# Add art.
-
-# Clear screen between rounds.
-
 from game_data import data
-import random
 from art import logo, vs
-from replit import clear
+import random
+import clear
 
+# Generate a random account from the game data.
 def get_random_account():
   """Get data from random account"""
   return random.choice(data)
 
+# Format account data into printable format.
 def format_data(account):
   """Format account into printable format: name, description and country"""
   name = account["name"]
@@ -37,6 +17,9 @@ def format_data(account):
   # print(f'{name}: {account["follower_count"]}')
   return f"{name}, a {description}, from {country}"
 
+# Check if user is correct.
+## Get follower count.
+## If Statement
 def check_answer(guess, a_followers, b_followers):
   """Checks followers against user's guess 
   and returns True if they got it right.
@@ -46,15 +29,17 @@ def check_answer(guess, a_followers, b_followers):
   else:
     return guess == "b"
 
-
 def game():
+  # Add art.
   print(logo)
   score = 0
   game_should_continue = True
   account_a = get_random_account()
   account_b = get_random_account()
 
+  # Make game repeatable.
   while game_should_continue:
+    # Make B become the next A.
     account_a = account_b
     account_b = get_random_account()
 
@@ -65,14 +50,17 @@ def game():
     print(vs)
     print(f"Against B: {format_data(account_b)}.")
     
+    # Ask user for a guess.
     guess = input("Who has more followers? Type 'A' or 'B': ").lower()
     a_follower_count = account_a["follower_count"]
     b_follower_count = account_b["follower_count"]
     is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
+    # Clear screen between rounds.
     clear()
     print(logo)
     if is_correct:
+      # Score Keeping.
       score += 1
       print(f"You're right! Current score: {score}.")
     else:
@@ -80,3 +68,5 @@ def game():
       print(f"Sorry, that's wrong. Final score: {score}")
 
 game()
+
+# Feedback.
